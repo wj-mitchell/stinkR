b2030080a9748cb0b4e4dc8b0cde41d40dae76fb
Wjpmitchell3 <62663636+Wjpmitchell3@users.noreply.github.com>
2022-01-13 22:00:05 -0500

    Update again.R

--- again.R
+++ again.R
@@ -1,7 +1,10 @@
 ## again.R function | v1.0 2022.01.02
 
 # This function will run any given function whose output is a value or vector 
-# nrep times and concatenate the values or vectors into a single vector
+# nrep times and concatenate the values or vectors into a single vector.
+
+# I primarily use it when running certain sampling functions, which would 
+# otherwise produce an array of the same values of nrep length. 
 
 again <- function(func,
                   arg,
